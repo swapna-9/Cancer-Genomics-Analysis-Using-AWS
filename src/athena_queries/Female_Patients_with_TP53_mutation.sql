d5d461ce2fdc068798d8c6c16550d8e0ea71f8db
swapna-9 <134816143+swapna-9@users.noreply.github.com>
2025-01-28 09:43:38 -0500

    Update Female_Patients_with_TP53_mutation.sql

---
+++ src/athena_queries/Female_Patients_with_TP53_mutation.sql
@@ -0,0 +1,18 @@
+SELECT 
+    s.sample_source,
+    s.location,
+    s.tumor,
+    s.normal,
+    v.type,
+    v.type_2,
+    v.annotated,
+    v.name
+FROM 
+    samples s
+LEFT JOIN 
+    vcf v 
+ON 
+    s.tumor = v.tumor
+WHERE 
+    sex = 'female' 
+    AND name = 'TP53';
